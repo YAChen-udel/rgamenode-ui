32fc232e60294ce29cae46d15af9dd92abe256ea
Nick Napior <nnapior@gmail.com>
2020-11-12 16:09:57 -0500

    moved nav to index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,

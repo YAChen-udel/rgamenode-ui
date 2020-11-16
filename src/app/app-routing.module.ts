eb0425a159ff1fedb39b8ad29769487a6390d641
Trevor Seidel <tside777@gmail.com>
2020-11-16 14:01:20 -0500

    initial game list page and game page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { GamesComponent } from './games/games.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home', component: HomeComponent},
-  {path:'about',component: AboutComponent}
+  {path:'about',component: AboutComponent},
+  {path:'games',component: GamesComponent},
+  {path:'games/:gameId',component: GameComponent}
 ];
 
 @NgModule({
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-  constructor() { }
+  mygames = [{'title':'game1', 'pic':'assets/img/0986f97be719fb9a.jpg', 'gameId':'1'},{'title':'game2', 'pic':'assets/img/6fe407b4b13bbcde.jpg', 'gameId':'2'},{'title':'game3', 'pic':'assets/img/8a26974ee6444acd.jpg', 'gameId':'3'}];
+  photo = {
+    style: {
+      width: '200px',
+      height: '200px'
+    }
+  };
 
-  ngOnInit(): void {
+  constructor() {
   }
+
+  ngOnInit(): void {}
 
 }

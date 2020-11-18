98c1b8f7f9cabddac204a2987ea57fd3e3f755a4
Trevor Seidel <tside777@gmail.com>
2020-11-18 17:57:18 -0500

    Connected games and game page to api with game service

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  game;
+  name;
+  creator;
+  owner;
+  thumbnail;
+  gamefiles;  //Not sure how this piece is gonna be implemented yet but we'll figure it out
+
+
+  constructor(private route: ActivatedRoute, private api: GameService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.getGameByID(params.get('gameID'));
+    });
+  }
+
+  getGameByID(gameID: string) {
+    this.api.getGameByID(gameID).subscribe((data) => {
+      console.log(data)
+      this.game = data["data"];
+      console.log(this.game)
+      this.name = this.game["name"];
+      console.log(this.name);
+      this.creator = this.game["creator"];
+      this.owner = this.game["owner"];
+    })
   }
 
 }
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameService } from '../services/game.service'
+
 
 @Component({
   selector: 'app-games',
@@ -7,17 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-  mygames = [{'title':'game1', 'pic':'assets/img/0986f97be719fb9a.jpg', 'gameId':'1'},{'title':'game2', 'pic':'assets/img/6fe407b4b13bbcde.jpg', 'gameId':'2'},{'title':'game3', 'pic':'assets/img/8a26974ee6444acd.jpg', 'gameId':'3'}];
-  photo = {
-    style: {
-      width: '200px',
-      height: '200px'
-    }
-  };
+  allgames = [];
 
-  constructor() {
+  constructor(private api: GameService) {}
+
+  ngOnInit() {
+    this.api.getGames().subscribe((data) => {
+      console.log(data);
+      this.allgames = data["data"];
+    })
   }
-
-  ngOnInit(): void {}
 
 }

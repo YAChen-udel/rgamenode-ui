24f64431e88dd3a917915bde30ec91fbdee68c9b
Nick Napior <nnapior@deployedresources.com>
2020-12-08 13:02:47 -0500

    file upload works

--- src/app/edit-game/edit-game.component.ts
+++ src/app/edit-game/edit-game.component.ts
@@ -13,6 +13,7 @@ export class EditGameComponent implements OnInit {
 
 	isLoaded = false;
 	game;
+	gameID:string;
 	name;
 	creatorID;
 	ownerID;
@@ -21,11 +22,9 @@ export class EditGameComponent implements OnInit {
 	creditIDs;
 	creditNames = [];
 	thumbnail;
-	gamefiles;  //Not sure how this piece is gonna be implemented yet but we'll figure it out
-	hasBaseDropZoneOver:boolean;
-	hasAnotherDropZoneOver:boolean;
-	uploader;
-	response:string;
+	hasBaseDropZoneOver: boolean;
+	uploader: FileUploader;
+	response: string;
 
 	constructor(private route: ActivatedRoute, private api: GameService, private authSvc: AuthService) {
 	}
@@ -33,38 +32,35 @@ export class EditGameComponent implements OnInit {
 	ngOnInit(): void {
 		this.route.paramMap.subscribe(params => {
 			this.getGameByID(params.get('gameID'));
+			this.gameID=params.get('gameID');
 		});
 		this.uploader = new FileUploader({
-			url: `https://localhost:3000/api/game/${this.game._id}/upload`,
-			disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-			formatDataFunctionIsAsync: true,
-			formatDataFunction: async (item) => {
-				return new Promise( (resolve, reject) => {
-					resolve({
-						name: item._file.name,
-						length: item._file.size,
-						contentType: item._file.type,
-						date: new Date()
-					});
-				});
-			}
+			url: `http://localhost:3000/api/game/${this.gameID}/upload`,
+			authToken: this.authSvc.token
 		});
 
+		this.uploader.onBeforeUploadItem = (item) => {
+  			item.withCredentials = false;
+		}
+
+		this.uploader.onAfterAddingFile = (item) => {
+  			item.method = "PUT";
+		}
+
 		this.hasBaseDropZoneOver = false;
-		this.hasAnotherDropZoneOver = false;
 
 		this.response = '';
 
 		this.uploader.response.subscribe( res => this.response = res );
+		this.isLoaded = true;
 	}
 
 	getGameByID(gameID: string) {
 		this.api.getGameByID(gameID).subscribe((data) => {
-			console.log(data)
+			console.log(data);
 			this.game = data["data"];
-			console.log(this.game)
+			console.log(this.game);
 			this.name = this.game["name"];
-			console.log(this.name);
 			this.creditIDs = this.game["credits"];
 			for(let credit of this.creditIDs) {
 				console.log(credit["id"]);
@@ -72,17 +68,11 @@ export class EditGameComponent implements OnInit {
 					console.log('credit data -->', data);
 					this.creditNames.push(data["data"]);
 					console.log(this.creditNames);
-				})}
-				this.isLoaded = true;
-			})
+				})}});
 		}
 
 		public fileOverBase(e:any):void {
 			this.hasBaseDropZoneOver = e;
-		}
-
-		public fileOverAnother(e:any):void {
-			this.hasAnotherDropZoneOver = e;
 		}
 
 	}
